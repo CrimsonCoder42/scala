6f020c0154cda75fa708266a59d268dac2765fb9
CrimsonCoder42 <nostro37@gmail.com>
2022-07-22 10:12:12 -0400

    add challenges from Java Masterclass to this

--- beginner_part_1/src/main/scala/lectures/part1Basics/Expressions.scala
+++ beginner_part_1/src/main/scala/lectures/part1Basics/Expressions.scala
@@ -22,7 +22,38 @@ object Expressions extends App {
   val aConditionedValue = if(aCondition) 5 else 3
   println(aConditionedValue)
 
+var i = 0
+  while(i < 10) {
+    println(i)
+    i += 1
+  }
+  //NEVER WRITE THIS AGAIN DO NOT WRITE IMPARATIVE CODE IN SCALA.
+
+  //EVERYTHING IN SCALA IS AN EXPRESSION!!
+
+  val aWeirdValue = (aVariable = 3)
+  println(aWeirdValue)
 
 
+  val aCodeBlock = {
+    val y = 2
+    val z = y + 1
+
+    if (z > 2) "hello" else "goodbye"
+  }
+
+
+  //1. println("Hello World will actually print to the counsel")
+    val someValue = {
+      2 < 3
+    }
+  //2.  true
+
+  val someOtherValue = {
+    if(someValue) 239 else 906
+    42
+  }
+
+  //3. 42
 
 }
